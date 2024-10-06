504a4c3e07b43075743370fe1372ceed36ba194e
Subhadra Mishra <subhadramishra1994@gmail.com>
2024-10-06 00:46:59 -0400

    Updated the Landing Page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
-// Import the LandingPage component
 import LandingPage from '../components/LandingPage'; // Adjust the path based on your project structure
 
-// Define the Home component
 export default function Home() {
   return (
     <div>
-      {/* Render the LandingPage component */}
       <LandingPage />
     </div>
   );
